d32b9007abe3f092b1c525c69545c330bbbcde2e
Wenrui Wu <donovan19921117@gmail.com>
2019-06-11 21:12:14 +0800

    added github repository address

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name="konsoru",
     version=__version__,
     author="Wenrui Wu",
-    url="",
+    url="https://github.com/DonovanWu/konsoru.git",
     description="A functional programming styled CLI console application framework based on argparse",
     packages=find_packages(exclude=['docs', 'examples', 'tests', '*.tests']),
     python_requires='>=3.5',
